dfe81a6600a9f33eae0ca3ebb29123d4b9f92574
lkamble5757-netizen <12345678+lkamble5757@gmail.com@users.noreply.github.com>
2025-11-06 14:58:18 +0530

    Claim Changes for CreatedBy Id

--- PrashantApi.Infrastructure/Constants/SqlMode.cs
+++ PrashantApi.Infrastructure/Constants/SqlMode.cs
@@ -24,26 +24,26 @@ namespace PrashantEle.API.PrashantEle.Infrastructure.Constants
 
     }
 
-    public partial class ClaimTypes
-    {
-        public const string Id = "id";
-        public const string FirstName = "firstName";
-        public const string LastName = "lastName";
-        public const string Name = "name";
-        public const string LoginId = "loginId";
-        public const string Email = "email";
-        public const string UserType = "userType";
-        public const string AreaId = "areaId";
-        public const string ReportingAuthorityId = "reportingAuthorityId";
-        public const string Role = "role";
-        public const string EmployeeCode = "employeeCode";
-        public const string RoleIds = "roleIds";
-        public const string Gender = "gender";
-        public const string IsPasswordChanged = "isPasswordChanged";
-        public const string PasswordChangedDate = "passwordChangedDate";
-        public const string Identifier = "identifier";
-        public const string Budget = "budget";
-        public const string CurrentDate = "currentDate";
-        public const string BranchCode = "BranchCode";
-    }
+    //public partial class ClaimTypes
+    //{
+    //    public const string Id = "Id";
+    //    public const string FirstName = "firstName";
+    //    public const string LastName = "lastName";
+    //    public const string Name = "name";
+    //    public const string LoginId = "loginId";
+    //    public const string Email = "email";
+    //    public const string UserType = "userType";
+    //    public const string AreaId = "areaId";
+    //    public const string ReportingAuthorityId = "reportingAuthorityId";
+    //    public const string Role = "role";
+    //    public const string EmployeeCode = "employeeCode";
+    //    public const string RoleIds = "roleIds";
+    //    public const string Gender = "gender";
+    //    public const string IsPasswordChanged = "isPasswordChanged";
+    //    public const string PasswordChangedDate = "passwordChangedDate";
+    //    public const string Identifier = "identifier";
+    //    public const string Budget = "budget";
+    //    public const string CurrentDate = "currentDate";
+    //    public const string BranchCode = "BranchCode";
+    //}
 }
